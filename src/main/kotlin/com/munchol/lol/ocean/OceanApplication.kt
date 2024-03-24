fa35526bdb32f88c7ce185deb244656a6eb4966a
turn <kksy2423@naver.com>
2024-03-24 21:58:20 +0900

    init commit

---
+++ src/main/kotlin/com/munchol/lol/ocean/OceanApplication.kt
@@ -0,0 +1,11 @@
+package com.munchol.lol.ocean
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OceanApplication
+
+fun main(args: Array<String>) {
+	runApplication<OceanApplication>(*args)
+}
---
+++ src/test/kotlin/com/munchol/lol/ocean/OceanApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.munchol.lol.ocean
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OceanApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
